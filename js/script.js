b5476e202e85362909a185dec094ed627209d85b
Radosław Orczykowski <radek.or@gmail.com>
2024-04-03 21:02:20 +0200

    add function generateTags

--- js/script.js
+++ js/script.js
@@ -35,7 +35,8 @@ function titleClickHandler(event) {
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
   // console.log("Coś ta funkcja robi");
@@ -83,3 +84,51 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+function generateTags() {
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
+
+  for (let article of articles) {
+    const wrapper = article.querySelector(optArticleTagsSelector);
+    // console.log(wrapper);
+    let html = '';
+    const articleTags = article.getAttribute('data-tags');
+    // console.log(articleTags);
+
+    const articleTagsArrey = articleTags.split(' ');
+    console.log(articleTagsArrey);
+
+    for (let tag of articleTagsArrey) {
+      const tagLinkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+      console.log(tagLinkHTML);
+    }
+  }
+}
+
+generateTags();
+
+function tagClickHandler(event) {
+  /* prevent default action for this event */
+  /* make new constant named "clickedElement" and give it the value of "this" */
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  /* make a new constant "tag" and extract tag from the "href" constant */
+  /* find all tag links with class active */
+  /* START LOOP: for each active tag link */
+  /* remove class active */
+  /* END LOOP: for each active tag link */
+  /* find all tag links with "href" attribute equal to the "href" constant */
+  /* START LOOP: for each found tag link */
+  /* add class active */
+  /* END LOOP: for each found tag link */
+  /* execute function "generateTitleLinks" with article selector as argument */
+}
+
+function addClickListenersToTags() {
+  /* find all links to tags */
+  /* START LOOP: for each link */
+  /* add tagClickHandler as event listener for that link */
+  /* END LOOP: for each link */
+}
+
+addClickListenersToTags();
